f145a90a32568fe622f445e4d68544ecff182115
Elvis <elvostudios@hotmail.com>
2020-03-24 16:51:35 +0100

    Added temporary database seeding method in context class itself

--- Data/WebbLab3Context.cs
+++ Data/WebbLab3Context.cs
@@ -11,8 +11,30 @@ namespace WebbLab3
         public WebbLab3Context (DbContextOptions<WebbLab3Context> options)
             : base(options)
         {
+            if (!Movie.Any()) { SeedDatabase(); }
         }
 
         public DbSet<WebbLab3.Movie> Movie { get; set; }
+
+        private void SeedDatabase()
+        {
+            Movie[] seedMovies =
+            {
+                new Movie
+                {
+                    Title = "Coolboi: Unleashed",
+                    StartTime = new DateTime(2020, 03, 24, 18, 30, 0),
+                    SeatsLeft = 50,
+                    Salon = 1
+                }
+            };
+
+            foreach (var movie in seedMovies)
+            {
+                Movie.Add(movie);
+            }
+
+            this.SaveChanges();
+        }
     }
 }
