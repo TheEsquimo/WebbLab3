c73d5e3783d10c95221c72ecc32b6211e1ce9e80
Elvis <elvostudios@hotmail.com>
2020-03-26 14:33:44 +0100

    Seed database function implemented

---
+++ Pages/Movies/SeedDatabase.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebbLab3
+{
+    public class SeedDatabaseModel : PageModel
+    {
+        private readonly WebbLab3.WebbLab3Context _context;
+        public bool seedSuccesfull = false;
+        public bool seedFailed = false;
+
+        public SeedDatabaseModel(WebbLab3.WebbLab3Context context)
+        {
+            _context = context;
+        }
+
+        public void OnPostSeedDatabase()
+        {
+            if (!_context.Movie.Any()) 
+            { 
+                _context.SeedDatabase();
+                seedSuccesfull = true;
+            }
+            else
+            {
+                seedFailed = true;
+            }
+        }
+    }
+}
